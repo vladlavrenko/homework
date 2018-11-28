479a1fb6fbd17e7143bee30c461a437feeb84d31
vladislav.lavrenko <vlavrenko.kh@gmail.com>
2018-11-28 16:36:08 +0200

    Созданы тесты для рдактирования и удаления контактов и групп

--- src/test/java/Appmanager/ApplicationManager.java
+++ src/test/java/Appmanager/ApplicationManager.java
@@ -1,7 +1,10 @@
 package Appmanager;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -12,9 +15,23 @@ public class ApplicationManager {
     private GroupHelper groupHelper;
     private SessionHelper sessionHelper;
     private ContactHelper contactHelper;
+    private String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
     public void init() {
-        driver = new FirefoxDriver();
+        if (browser.equals(BrowserType.FIREFOX)) {
+            driver = new FirefoxDriver();
+        } else if (browser.equals(BrowserType.CHROME)) {
+            driver = new ChromeDriver();
+        } else if (browser.equals(BrowserType.EDGE)) {
+            driver = new EdgeDriver();
+        } else {
+            System.out.println("Incorrect browser");
+        }
+
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         navigationHelper = new NavigationHelper(driver);
         sessionHelper = new SessionHelper(driver);
--- src/test/java/Tests/TestBase.java
+++ src/test/java/Tests/TestBase.java
@@ -1,12 +1,13 @@
 package Tests;
 
 import Appmanager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 public class TestBase {
 
-    final ApplicationManager app = new ApplicationManager();
+    final ApplicationManager app = new ApplicationManager(BrowserType.EDGE);
 
     @BeforeClass(alwaysRun = true)
     public void setUp() {
