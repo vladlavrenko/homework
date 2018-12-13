44ed13de642a15ec7a57e246651d77eece6ef53d
vladislav.lavrenko <vlavrenko.kh@gmail.com>
2018-12-13 12:57:55 +0200

    сравнение списка групп до и после удаления

--- src/test/java/Tests/DeleteGroupTest.java
+++ src/test/java/Tests/DeleteGroupTest.java
@@ -22,9 +22,8 @@ public class DeleteGroupTest extends TestBase {
         Assert.assertEquals(after.size(), before.size() - 1);
 
         before.remove(before.size() - 1);
-        for (int i = 0; i < after.size(); i++) {
-            Assert.assertEquals(before.get(i), after.get(i));
-        }
+
+        Assert.assertEquals(before, after);
 
     }
 }
